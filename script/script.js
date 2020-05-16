bdfa4292d93ab71d8940a109cfed422ac7102358
Anton Varlamkin <41117155+Kjaly@users.noreply.github.com>
2020-05-16 18:09:18 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -6,6 +6,84 @@ document.addEventListener('DOMContentLoaded', function () {
     const questionTitle = document.querySelector('#question');
     const formAnswers = document.querySelector('#formAnswers');
     const burgerBtn = document.getElementById('burger');
+    const nextBtn = document.getElementById('next');
+    const prevBtn = document.getElementById('prev');
+
+    const questions = [
+        {
+            question: "Какого цвета бургер?",
+            answers: [
+                {
+                    title: 'Стандарт',
+                    url: './image/burger.png'
+                },
+                {
+                    title: 'Черный',
+                    url: './image/burgerBlack.png'
+                }
+            ],
+            type: 'radio'
+        },
+        {
+            question: "Из какого мяса котлета?",
+            answers: [
+                {
+                    title: 'Курица',
+                    url: './image/chickenMeat.png'
+                },
+                {
+                    title: 'Говядина',
+                    url: './image/beefMeat.png'
+                },
+                {
+                    title: 'Свинина',
+                    url: './image/porkMeat.png'
+                }
+            ],
+            type: 'radio'
+        },
+        {
+            question: "Дополнительные ингредиенты?",
+            answers: [
+                {
+                    title: 'Помидор',
+                    url: './image/tomato.png'
+                },
+                {
+                    title: 'Огурец',
+                    url: './image/cucumber.png'
+                },
+                {
+                    title: 'Салат',
+                    url: './image/salad.png'
+                },
+                {
+                    title: 'Лук',
+                    url: './image/onion.png'
+                }
+            ],
+            type: 'checkbox'
+        },
+        {
+            question: "Добавить соус?",
+            answers: [
+                {
+                    title: 'Чесночный',
+                    url: './image/sauce1.png'
+                },
+                {
+                    title: 'Томатный',
+                    url: './image/sauce2.png'
+                },
+                {
+                    title: 'Горчичный',
+                    url: './image/sauce3.png'
+                }
+            ],
+            type: 'radio'
+        }
+    ];
+
 
     let  clientWidth =document.documentElement.clientWidth;
     if (clientWidth < 768){
@@ -53,23 +131,52 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     const playTest = () => {
-        const renderQuestions = () => {
-            questionTitle.textContent = 'Какого цвета бургер вы хотите?';
-            const name = 'Стандарт';
-            const img = './image/burger.png';
-
-            formAnswers.innerHTML = `
-              <div class="answers-item d-flex flex-column">
-                <input type="radio" id="answerItem1" name="answer" class="d-none">
-                <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                  <img class="answerImg" src=${img} alt="burger">
-                  <span>${name}</span>
+        let numberQuestions = 0;
+        const renderAnswers = (index) => {
+            questions[index].answers.forEach((answer, index, arr) => {
+                const answerItem = document.createElement('div');
+                answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
+                answerItem.innerHTML = `
+                <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+                <label for="${answer.title}" class="d-flex flex-column justify-content-between">
+                  <img class="answerImg" src=${answer.url} alt="burger">
+                  <span>${answer.title}</span>
                 </label>
-              </div>
-              
-        `
+                `
+                formAnswers.appendChild(answerItem);
+            })
+            console.log(questions.length);
+        }
+        const renderQuestions = (indexQuestion) => {
+            formAnswers.innerHTML = ``;
+            questionTitle.textContent = `${questions[indexQuestion].question}`;
+            renderAnswers(indexQuestion);
+            if (numberQuestions == 0){
+                prevBtn.style.display = 'none'
+            } else {
+                prevBtn.style.display = 'flex';
+            }
+
+            if (numberQuestions == questions.length-1) {
+                nextBtn.style.display = 'none';
+            } else {
+                nextBtn.style.display = 'flex';
+            }
+        }
+        renderQuestions(numberQuestions);
+
+        nextBtn.onclick = () => {
+            numberQuestions++;
+            renderQuestions(numberQuestions);
+        }
+        prevBtn.onclick = () => {
+            numberQuestions--;
+            renderQuestions(numberQuestions);
 
         }
-        renderQuestions();
+
     }
-})
+
+
+
+});
